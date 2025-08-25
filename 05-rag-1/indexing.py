ec89a246b9a7b14d00f1cf1207eb130a103fd848
Akash Malhotra <akashkumarmalhotra@gmail.com>
2025-08-25 16:05:39 -0500

    A small snippet on RAG

--- 05-rag-1/indexing.py
+++ 05-rag-1/indexing.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from dotenv import load_dotenv
+from langchain_qdrant import QdrantVectorStore
 
 load_dotenv()
 
@@ -25,3 +26,11 @@ embedding_model = OpenAIEmbeddings(
 )
 
 #Using embedding_model create embedding of split_text and store it in DB
+vector_store = QdrantVectorStore.from_documents(
+    documents=split_text,
+    url ="http://localhost:6333",
+    collection_name ="learning_vectors",
+    embedding=embedding_model
+)
+
+print("Indexing of Document Done....")
